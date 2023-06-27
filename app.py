724788e876a7ef20b0f514338e6dc7766fe1c600
Pamir khan <pmysteryk@gmail.com>
2023-06-27 15:59:32 +0500

    feat; add name request variable

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello_world(name='Pamir'):
+    name = request.args.get('name', name)
+    return 'Hello {}!'.format(name)
 
 
 if __name__ == '__main__':

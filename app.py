2f244c4d1035c93a134c2bfffa623c703ecd78fc
Pamir khan <pmysteryk@gmail.com>
2023-06-27 16:01:39 +0500

    feat: clean url arguments

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/<name>')
 def hello_world(name='Pamir'):
-    name = request.args.get('name', name)
     return 'Hello {}!'.format(name)
 
 

47af36c7fd7875ee6e29018b9e2805410586fa79
Pamir khan <pmysteryk@gmail.com>
2023-06-28 13:50:50 +0500

    feat: added builder view function and template

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import json
 
 from flask import Flask, render_template, redirect, url_for, request, make_response
 
+from options import DEFAULTS
+
 app = Flask(__name__)
 
 
@@ -21,11 +23,20 @@ def index():
 
 @app.route('/save', methods=['POST'])
 def save():
-    response = make_response(redirect(url_for('index')))
+    response = make_response(redirect(url_for('builder')))
     data = get_saved_data()
     data.update(dict(request.form.items()))
     response.set_cookie('character', json.dumps(data))
     return response
+
+
+@app.route('/builder')
+def builder():
+    return render_template(
+        'builder.html',
+        saves=get_saved_data(),
+        options=DEFAULTS
+    )
 
 
 if __name__ == '__main__':

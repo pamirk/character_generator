7b5064d265c83a3226f981b57545c7dd783fed68
Pamir khan <pmysteryk@gmail.com>
2023-06-28 12:18:24 +0500

    feat: add index.html and view function

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/<name>')
-def hello_world(name='Pamir'):
-    return 'Hello {}!'.format(name)
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
